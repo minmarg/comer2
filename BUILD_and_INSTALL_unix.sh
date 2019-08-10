c49fec7a1f3f29c10a2de2b8b945fb47f330c82c
Mindaugas Margelevicius <mindaugas.margelevicius@bti.vu.lt>
2019-08-10 20:02:55 +0300

    Initial Commit

--- BUILD_and_INSTALL_unix.sh
+++ BUILD_and_INSTALL_unix.sh
@@ -2,7 +2,7 @@
 
 echo
 
-MYHOMEDEF=/mnt${HOME:1}/projects/share/cuda-comer
+MYHOMEDEF=${HOME}/local/comer2
 
 read -ep "Enter COMER2 install path: " -i "${MYHOMEDEF}" MYHOME
 
@@ -13,13 +13,10 @@ echo
 cd build || exit 1
 
 
-##cmake -DCMAKE_C_COMPILER=/home/mindaugas/bin/gcc -DCMAKE_CXX_COMPILER=/home/mindaugas/bin/c++ -DCMAKE_INSTALL_PREFIX=${HOME}/projects/share/cuda-comer ../src/
-#cmake -DCMAKE_INSTALL_PREFIX=/mnt${HOME:1}/projects/share/cuda-comer -DCMAKE_CUDA_FLAGS="-arch=sm_50" ../src/
-#cmake -DCMAKE_INSTALL_PREFIX=/mnt${HOME:1}/projects/share/cuda-comer -DCMAKE_CUDA_FLAGS="-gencode arch=compute_50,code=sm_50 -gencode arch=compute_60,code=sm_60" ../src/
 cmake -DCMAKE_INSTALL_PREFIX=${MYHOME} \
     -DCMAKE_CUDA_FLAGS="-gencode arch=compute_35,code=sm_35 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_70,code=sm_70" \
     ../src/  ||  (cd ..; exit 1)
-#consider option: -DCMAKE_VERBOSE_MAKEFILE=ON
+#option: -DCMAKE_VERBOSE_MAKEFILE=ON
 
 cmake --build . --config Release --target install  ||  (cd ..; exit 1)
 
